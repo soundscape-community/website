b5012e1aa30d57fe8ec92abef3579d1c2b650a82
sheldonej <56203719+sheldonej@users.noreply.github.com>
2024-07-14 08:01:14 -0400

    Fixing file extension

--- static/images/page.tsx
+++ static/images/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import CampAbilitiesCoverImage from "@/static/images/cover-image.jpg"
+import CampAbilitiesCoverImage from "@/static/images/camp-abilities/cover-image.jpg"
 import Image from "next/image"
 import Link from "next/link";
 
